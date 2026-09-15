a84b0ca2fbf1f9583b7a44369f12bd42bc87a482
Toto2807 <146028794+Toto2807@users.noreply.github.com>
2026-09-15 13:44:10 +0200

    app.js : changement du Text en text

--- public/js/app.js
+++ public/js/app.js
@@ -53,7 +53,7 @@ formulaire?.addEventListener(`submit`, (event) => {
   }else{
       const message_envoye = validation.value
       const reponse = replyTo(message_envoye)
-      historique.push({ role: 'user', Text: message_envoye })
+      historique.push({ role: 'user', text: message_envoye })
       historique.push({ role: 'assistant', text: reponse })
       localStorage.setItem('capweb.historique',JSON.stringify(historique))
       renderMessages(historique, messages)
